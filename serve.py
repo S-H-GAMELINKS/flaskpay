e02c9dd375b33197ed2ea9acee758dea7255965c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-07 13:27:18 +0900

    add & render index.html

--- serve.py
+++ serve.py
@@ -1,10 +1,10 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template('static/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
