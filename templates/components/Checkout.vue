e84924f99260e42c6e8911d7f36fda3189a8a99e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-07 13:59:23 +0900

    fix Checkout.vue

--- templates/components/Checkout.vue
+++ templates/components/Checkout.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        <h1>Hello</h1>
-    </div>
+  <div class="container">
+    <payjp-checkout
+      :api-key="public_key"
+      :client-id="client_id"
+      text="add credit crad"
+      submit-text="カードで支払い"
+      name-placeholder="JOHN DOE"
+      v-on:created="onTokenCreated"
+      v-on:failed="onTokenFailed"
+    ></payjp-checkout>
+  </div>
 </template>
+
+ <script>
+import PayjpCheckout from "vue-payjp-checkout";
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      public_key: process.env.PAYJP_PUBLIC_KEY,
+      client_id: process.env.PAYJP_CLIENT_ID
+    };
+  },
+  components: {
+    PayjpCheckout
+  },
+  methods: {
+    onTokenCreated(token) {
+      this.setCreditToken(token.id);
+    },
+    onTokenFailed(e) {
+      console.error(e);
+    },
+    setCreditToken: function(token) {
+      axios.post("/payment", { token: token }).then(
+        response => {
+          console.log(response);
+          alert("Success!");
+        },
+        error => {
+          console.log(error);
+          alert("Failed!");
+        }
+      );
+    }
+  }
+};
+</script>
