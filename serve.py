21a9c18952e700632d67419101a8a65323c47af2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-07 14:42:54 +0900

    add /payment

--- serve.py
+++ serve.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import payjp
+import os
 
 app = Flask(__name__)
+
+SECRET_KEY = 'b'
+PUBLIC_KEY = ''
+
+payjp.api_key = ''
 
 @app.route('/')
 def index():
@@ -9,6 +16,24 @@ def index():
 @app.route('/index.js')
 def index_js():
     return render_template('index.js')
+
+@app.route('/payment', methods=['POST'])
+def payment():
+    print(request.json.get('token'))
+    amount = 1000
+    customer = payjp.Customer.create(
+        card=request.json.get('token')
+    )
+
+    payjp.Charge.create(
+        amount=amount,
+        currency='jpy',
+        customer=customer.id,
+        description='flask example charge'
+    )
+
+    return "success"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
